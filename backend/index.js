87ab95ec78a0ece2c0ff827096bee98c44dcdc4f
Daniel <danibociat@gmail.com>
2021-11-13 13:49:34 +0200

    Added the basic backend structure

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Works");
+});
+
+// PORT
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`));

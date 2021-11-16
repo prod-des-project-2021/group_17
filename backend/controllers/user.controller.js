3429578affce7cf6d200a071207a14725d2171e0
ValentinRL <valentin.alt.raltchev99@gmail.com>
2021-11-16 09:34:29 +0100

    User Implementation Example

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,18 @@
+const addUser = async (req, res, next) => {
+  const {user, content} = req.body
+// TODO
+//   try {
+//     await createBlogpost(user, content)
+//     // other service call (or same service, different function can go here)
+//     // i.e. - await generateBlogpostPreview()
+//     res.sendStatus(201)
+//     next()
+//   } catch(e) {
+//     console.log(e.message)
+//     res.sendStatus(500) && next(error)
+//   }
+}
+
+module.exports = {
+    addUser
+}

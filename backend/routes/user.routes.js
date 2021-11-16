8399969d72f34e36b457be1b50f75452f61e2077
ValentinRL <valentin.alt.raltchev99@gmail.com>
2021-11-16 09:34:29 +0100

    User Implementation Example

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/user.controller')
 
-//TODO
+router.post('/addUser', userController.addUser)
 
 module.exports = router

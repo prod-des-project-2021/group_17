dbf96160a4625b177a0b6f9970b3327b99881d7f
LisaSophiaSchmidt <ls170@hdm-stuttgart.de>
2021-11-18 10:38:43 +0200

    Workday 18.11.2021
    Please do not do anything with the commit or request! Just leave it as this - it is still full of errors I'll first have to clean out.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SignInScreen from './login/SignIn';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Container, ThemeProvider, CssBaseline, Button } from '@material-ui/core';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './login/firebaseconfig';
+import Theme from './Theme';
+import Header from './components/homepage/Header';
+import Footer from './components/homepage/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		// currentUser wird von Google geliefert
+		// apiUser ist der User aus unserer User aus der API
+		this.state = { //vgl self in python
+			currentUser : null,
+			apiUser : null,
+			appError: null,
+			authError: null,
+			authLoading: false,
+			newUser: false,
+		};
+		//initialize firebase if its not already running
+		if(!firebase.apps.length)  firebase.initializeApp(firebaseConfig);
+		firebase.auth().languageCode = 'de';
+		firebase.auth().onAuthStateChanged(this.handleAuthStateChange); //eventhandler für login/logout
+	}
+  /** Renders the whole app */
+	render() {
+		const { appError, currentUser, apiUser}= this.state;
+		return (
+
+			<ThemeProvider theme={Theme}>
+				<CssBaseline />
+				<Router basename={process.env.PUBLIC_URL}> {/* fügt automatisch die url vor jedem link ein */}
+					<Header user={apiUser}/>
+					<Container maxWidth='xl'>
+						{	
+            
+							// Is a user signed in?
+							currentUser ? //todo: ist user neu?
+							!apiUser?<>Loading...</>:
+							apiUser.returnIsNewUser() ?
+							<>
+							<Settings signup={true} currentUser={currentUser} apiUser={apiUser}/>
+							</>
+							:
+								// normal Routes
+								<>
+									
+									<Redirect from='/' to='basesite' />
+									<Route exact path='/basesite' component={Redirect} >
+										<p>Homepage</p>
+										<p>Hello  {currentUser.displayName }</p>
+										<p>Database UserID: {apiUser ? apiUser.getID():""}</p>
+										<Button onClick={this.handleSignOut}>Logout</Button>
+										
+									</Route>
+									<Route path='/profil'>
+									{apiUser?<Profil currentUser={apiUser}/>:null}
+									</Route>
+									<Route path='/search' >
+										{apiUser?<Search currentUser={apiUser}/>:null}
+									</Route>
+									<Route path='/cart'>
+										{apiUser?<Cart currentUser={apiUser}/>:null}
+									</Route>
+								<Footer />	
+								</>
+								:
+								// else show the sign in page
+								<>
+									<Redirect to='/index.html' />
+									<SignInScreen onNewUser={this.handleNewUser.bind(this)} />
+								</>
+						}
+						<ErrorHandler appError={appError} appErrorMsg={appError ? appError.text:null} />
+					</Container>
+				</Router>
+			</ThemeProvider>
+		);
+	}
 }
 
 export default App;
+

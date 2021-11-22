02ad5c882aaf4fa8b99bf560a57fdff097e9c0c4
LisaSophiaSchmidt <ls170@hdm-stuttgart.de>
2021-11-22 12:09:55 +0200

    Started with the homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,29 @@
+import React from "react";
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+            userId: null,
+            userName: null,
+            userEmail: null,
+            userPassword: null,
+            userPasswordConfirm: null,
+        };
+    }
+
+    render() {
+		const { user } = this.state;
+        return (
+            <div>
+                <h1>Hi <p>{user}</p> and welcome to PSOAStore</h1>
+                <p>
+                    Please sign in: {user ? user.name : "not logged in"}.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;

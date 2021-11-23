f1b3a98147fe812867bb151a2f5f7169e0c88286
Daniel <danibociat@gmail.com>
2021-11-23 12:09:19 +0200

    Finalized raw User services, without error handling

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user.controller')
 
-router.post('/addUser', userController.addUser)
+router.post('/', userController.addUser)
+router.get('/', userController.getUser)
+router.get('/:uid', userController.getUserbyId)
+router.delete('/:uid', userController.deleteUser)
+router.put('/:uid', userController.updateUser)
 
 module.exports = router
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,4 +1,5 @@
 const userDB = require("../models/User");
+const User = require("../models/User");
 
 const createUser = async (content) => {
     const usr = userDB.build(content);
@@ -7,6 +8,35 @@ const createUser = async (content) => {
     console.log("User saved");
 }
 
+const fetchUsers = async() => {
+    //console.log(await  User.findAll())    
+    
+    return await User.findAll({attributes:{exclude: ['password']}}).then(function (users) {
+        return users.map(function(obj) {return obj.dataValues});
+    });
+}
+
+const fetchUserByID = async(uid) => {
+    return await User.findOne({where: {id: uid}, attributes: {exclude: ['password']}}).then(function (user) {
+        if(!user)
+            return null;
+        else
+            return user.dataValues;
+    });
+}
+
+const removeUser = async(uid) => {
+    await User.destroy({where: {id: uid}});
+}
+
+const editUser = async(content, uid) => {
+    await User.update({first_name: content.first_name}, {where: {id: uid}});
+}
+
 module.exports = {
-    createUser
+    createUser,
+    fetchUsers,
+    fetchUserByID,
+    removeUser,
+    editUser
 }

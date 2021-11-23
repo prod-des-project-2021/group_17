a58fb5383de8031c9c7947a8e8e2ccea68a13c6b
ValentinRL <valentin.alt.raltchev99@gmail.com>
2021-11-23 11:16:39 +0100

    Register and Login Implement v1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const user = require('./user.routes')
 const product = require('./product.routes')
 const order = require('./order.routes')
+const auth = require('./auth.routes')
 
-router.use('/user',user);
+router.use('/user',user)
 router.use('/product',product)
 router.use('/order',order)
+router.use('/auth',auth)
 
 module.exports = router

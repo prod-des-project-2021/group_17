1b7a7e5c99cf6fe2ce381d09871d2039ab94eed9
LisaSophiaSchmidt <ls170@hdm-stuttgart.de>
2021-11-25 15:03:24 +0200

    Update ContactUs.js

--- frontend/src/pages/ContactUs.js
+++ frontend/src/pages/ContactUs.js
@@ -1,8 +1,8 @@
 import { Container } from '@mui/material'
 import React from 'react'
 import { ContentElement } from '../components/navbar/ContentElement'
-import PhoneIcon from '@mui/icons-material/Phone';
-import MailIcon from '@mui/icons-material/Mail';
+/* import PhoneIcon from '@mui/icons-material/Phone';
+import MailIcon from '@mui/icons-material/Mail'; */
 
 const Contactus = () => {
     return (
@@ -17,7 +17,7 @@ const Contactus = () => {
                         height: '90vh'
                     }}
                 > */}
-                    <h3> Hi! We are happy to have personal contact with you right away. <br /> </h3>
+                    <h3> Hi 😀 We are happy to have personal contact with you right away! <br /> </h3>
                     <br/>
                     <p> We hope there were no problems and your purchased product arrived in good condition. <br/>
                         But also in case of any problems we will be glad to help you.<br/>
@@ -27,19 +27,21 @@ const Contactus = () => {
                         <br/>
                         Our service staff will be happy to assist you.<br/>
                         <br/>
-                        Mon-Fri: 8 a.m. - 6 p.m.<br/>
-                        Sat: 10 a.m. - 6 p.m.<br/>
-                        Sun: Time to relax!<br/>
                     </p>
+                    <h4> 
+                        Mon - Fri: 8 a.m. - 6 p.m.<br/> 
+                        Sat: 10 a.m. - 6 p.m.<br/>
+                    </h4>
                     <br/>
                     <br/>
                     <br/>
-                    <h1>{<PhoneIcon />} </h1>
+                    {/* <h1>{<PhoneIcon />} </h1> */}
+                    <h1>☎️</h1>
                     <p>Phone: 0123456789</p>
                     <br />
                     <br />
-                    <br />
-                    <h1>{<MailIcon />}</h1>
+                    {/* <h1>{<MailIcon />}</h1> */}
+                    <h1>📧</h1>
                     <p>E-Mail: example@example.com</p>
                     <br />
                     <p>(Please note: When contacting us by email, remember to include your full name and, if applicable, your order number in case you have any problems with a purchased product.)</p>

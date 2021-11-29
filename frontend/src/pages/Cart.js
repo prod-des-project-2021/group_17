1a0ed7e9dad11c3413b3d4d65ccb9002f9c55997
LisaSophiaSchmidt <ls170@hdm-stuttgart.de>
2021-11-29 12:23:04 +0200

    Stepper in Cart.js implemented

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,9 +1,147 @@
-import { Container } from '@mui/material'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
+import { ContentElement } from '../components/navbar/ContentElement';
+
+const steps = [
+    'Add personal data',
+    'Promotion codes',
+    'Add payment method',
+    'View your order',
+    'Place your order'];
+
+function HorizontalLinearStepper() {
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [skipped, setSkipped] = React.useState(new Set());
+
+    const isStepOptional = (step) => {
+        return step === 1;
+    };
+
+    const isStepSkipped = (step) => {
+        return skipped.has(step);
+    };
+
+    const handleNext = () => {
+        let newSkipped = skipped;
+        if (isStepSkipped(activeStep)) {
+            newSkipped = new Set(newSkipped.values());
+            newSkipped.delete(activeStep);
+        }
+
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setSkipped(newSkipped);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleSkip = () => {
+        if (!isStepOptional(activeStep)) {
+            // You probably want to guard against something like this,
+            // it should never occur unless someone's actively trying to break something.
+            throw new Error("You can't skip a step that isn't optional.");
+        }
+
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setSkipped((prevSkipped) => {
+            const newSkipped = new Set(prevSkipped.values());
+            newSkipped.add(activeStep);
+            return newSkipped;
+        });
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
+    return (
+        <Container>
+            <ContentElement>
+                <Box sx={{ width: '100%' }}>
+                    <Stepper activeStep={activeStep}>
+                        {steps.map((label, index) => {
+                            const stepProps = {};
+                            const labelProps = {};
+                            if (isStepOptional(index)) {
+                                labelProps.optional = (
+                                    <Typography variant="caption">Optional</Typography>
+                                );
+                            }
+                            if (isStepSkipped(index)) {
+                                stepProps.completed = false;
+                            }
+                            return (
+                                <Step key={label} {...stepProps}>
+                                    <StepLabel {...labelProps}>{label}</StepLabel>
+                                </Step>
+                            );
+                        })}
+                    </Stepper>
+                    {activeStep === steps.length ? (
+                        <React.Fragment>
+                            <Typography sx={{ mt: 2, mb: 1 }}> <br />
+                            <br />
+                            <h1 >Congratulations! We have just received your order. </h1> <br />
+                            We will send you a confirmation email as soon as possible and contact you when your order is on its way.
+                            </Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Box sx={{ flex: '1 1 auto' }} />
+                                <Button onClick={handleReset}>Reset</Button>
+                            </Box>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Button
+                                    color="inherit"
+                                    disabled={activeStep === 0}
+                                    onClick={handleBack}
+                                    sx={{ mr: 1 }}
+                                >
+                                    Back
+                                </Button>
+                                <Box sx={{ flex: '1 1 auto' }} />
+                                {isStepOptional(activeStep) && (
+                                    <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                                        Skip
+                                    </Button>
+                                )}
+
+                                <Button onClick={handleNext}>
+                                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                                </Button>
+                            </Box>
+                        </React.Fragment>
+                    )}
+                </Box>
+            </ContentElement>
+        </Container>
+    );
+}
+
+export default HorizontalLinearStepper;
+
+
+// Alternative Stepper with Icon and without the skipp steps
+
+/* import { Container } from '@mui/material'
 import React from 'react'
 import { ContentElement } from '../components/navbar/ContentElement'
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel'; */
 
 
-const Cart = () => {
+/* const Cart = () => {
     return (
         <Container>
             <ContentElement>
@@ -22,4 +160,33 @@ const Cart = () => {
     )
 }
 
-export default Cart 
+export default Cart */
+
+
+/*
+const steps = [
+    'View your order',
+    'Add personal data',
+    'Add payment method',
+    'Place your order'
+];
+
+
+function Cart() {
+    return (
+        <Container>
+            <ContentElement>
+                <Box sx={{ width: '100%'}}>
+                    <Stepper sx={{ color: 'green'}}activeStep={0} alternativeLabel >
+                        {steps.map((label) => (
+                            <Step key={label} >
+                                <StepLabel>{label}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Box>
+            </ContentElement>
+        </Container>
+    );
+}
+export default Cart */

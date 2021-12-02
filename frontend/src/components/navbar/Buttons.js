687293362f2cbf43cec684cc849119cc6b92f607
LisaSophiaSchmidt <ls170@hdm-stuttgart.de>
2021-12-02 15:10:52 +0200

    Login implemented

---
+++ frontend/src/components/navbar/Buttons.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { NavLink as Link } from 'react-router-dom';
+
+
+/* export const NavButton = styled.nav`
+    position: fixed;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    padding: 4px 60px;
+    z-index: 10;
+`; */
+
+export const LoginButton = styled.nav`
+    display: flex;
+    align-items: center;
+    align-content: center;
+    margin-right: 24px;
+    justify-content: space-between;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+export const LoginButtonLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    align-content: center;
+    border-radius: 4px;
+    background: #006600;     
+    padding: 10px 22px;
+    color: #fff;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #8c8c8c;
+        color: #010606;
+    }
+`;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -511,3 +511,16 @@ function Homepage() {
 export default Homepage
 
 
+/* import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
+export default function ImageAvatars() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+    </Stack>
+  );
+} */

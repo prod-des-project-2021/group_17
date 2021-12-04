c5d6b1fd942c89574ad39ddd38793659e7b4c14d
Daniel <danibociat@gmail.com>
2021-12-04 11:31:31 +0200

    Services done with basic authorization checking

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -2,12 +2,11 @@ const { connectionManager } = require("../config/db");
 const Product = require("../models/Product");
 const { use } = require("../routes");
 const productService = require("../services/product.service")
-const {createProduct} = productService
+const {createProduct, fetchProduct, fetchProductById, removeProduct, editProduct} = productService
 
 const addProduct = async (req, res, next) => {
-    const content = req.body;
     try {
-      await createProduct(content)
+      await createProduct(req)
       res.sendStatus(201)
       next()
     } catch(e) {
@@ -15,10 +14,81 @@ const addProduct = async (req, res, next) => {
       res.send(e.message)
       console.log(e.message)
     }
-  }
+};
 
+const getProduct = async (req, res, next) => {
+  try {
+		prod = await fetchProduct();
+    res.status(200);
+		res.send(prod);
+		next();
+	} catch (e) {
+		console.log(e.message);
+		res.sendStatus(400);
+	}
+};
 
+const getProductbyId = async (req, res, next) => {
+	try {
+		prod = await fetchProductById(req.params.pid);
+
+		if (!prod) {
+			res.sendStatus(404);
+		} else {
+			res.status(200);
+			res.send(prod);
+		}
+		next();
+	} catch (e) {
+		console.log(e.message);
+		res.sendStatus(400);
+	}
+};
+
+const deleteProduct = async (req, res, next) => {
+	try {
+    prod = await fetchProductById(req.params.pid);
+
+    if(prod.user_id == req.user.id || req.user.isAdmin){
+      await removeProduct(req.params.pid);
+      res.sendStatus(200);
+    }
+    else{
+      res.sendStatus(401);
+    }
+		next();
+	} catch (e) {
+		console.log(e.message);
+		res.sendStatus(400);
+	}
+};
+
+const updateProduct = async (req, res, next) => {
+	try {
+    prod = await fetchProductById(req.params.pid);
+
+    if(!prod){
+       res.sendStatus(404);
+    }
+    else{
+      if(prod.user_id == req.user.id || req.user.isAdmin){
+        await editProduct(req.body, req.params.pid);
+        res.sendStatus(200);
+      }
+      else{
+        console.log("A");
+        res.sendStatus(401);
+      }
+    }
+    
+		
+		next();
+	} catch (e) {
+		console.log(e.message);
+		res.sendStatus(400);
+	}
+};
 
 module.exports = {
-    addProduct
+    addProduct, getProduct, getProductbyId, deleteProduct, updateProduct
 }

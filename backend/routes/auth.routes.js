a3af09f203025ed7f3549830bee70bdc00e34682
ValentinRL <valentin.alt.raltchev99@gmail.com>
2021-12-06 10:07:58 +0100

    Fix Changes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { login,register } = require('../controllers/auth.controller')
+const { login,register,token } = require('../controllers/auth.controller')
+const {authenticateJWT} = require("../utils/auth")
 
 router.post('/login', login)
 router.post('/register', register)
+router.post('/me',authenticateJWT, token)
 
 module.exports = router

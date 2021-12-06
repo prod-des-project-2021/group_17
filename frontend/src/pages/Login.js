5e84d78d53c4f95f11d315d992fee056e1261791
LisaSophiaSchmidt <ls170@hdm-stuttgart.de>
2021-12-06 18:23:12 +0200

    tested something out

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 //import { NavButton } from '../components/navbar/NavbarElement';
-import React from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Container } from '@mui/material'
 import { ContentElement } from '../components/navbar/ContentElement'
 import Box from '@mui/material/Box';
@@ -7,10 +8,33 @@ import TextField from '@mui/material/TextField';
 
 import { LoginButton, LoginButtonLink } from '../components/navbar/Buttons'
 
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
+   }
 
 
+function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+  
+    const handleSubmit = async e => {
+      e.preventDefault();
+      const token = await loginUser({
+        username,
+        password
+      });
+      console.log("hah")
+      setToken(token);
+    }
 
-function Login() {
+    
     
     return (
         <Container>
@@ -32,7 +56,7 @@ function Login() {
                         height: '30vh'
                     }}
                 >
-                    <Box
+                    <Box onSubmit={handleSubmit}
                         component="form"
                         sx={{
                             '& .MuiTextField-root': { m: 1, width: '25ch' },
@@ -41,7 +65,7 @@ function Login() {
                         autoComplete="off"
                     >
                         <div>
-                            <TextField
+                            <TextField onChange={e => setUserName(e.target.value)}
                                 required
                                 id="outlined-required"
                                 label="Email"
@@ -49,7 +73,7 @@ function Login() {
                             />
                         </div>
                         <div>
-                            <TextField
+                            <TextField onChange={e => setPassword(e.target.value)}
                                 id="outlined-password-input"
                                 label="Password"
                                 type="password"
@@ -72,3 +96,11 @@ function Login() {
 }
 
 export default Login
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+  };
+
+
+
+

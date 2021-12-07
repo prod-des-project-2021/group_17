646834c3cc1c5f066f7648ee79855a9427c225c0
Daniel <danibociat@gmail.com>
2021-12-07 17:11:43 +0200

    Improved and modified product/order services/controllers + checkout

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const user = require('./user.routes');
 const product = require('./product.routes');
-//const order = require('./order.routes')
+const order = require('./order.routes')
 const auth = require('./auth.routes')
 
 router.use('/user',user)
 router.use('/product',product)
-//router.use('/order',order)
+router.use('/order',order)
 router.use('/auth',auth)
 
 module.exports = router
--- backend/services/order.service.js
+++ backend/services/order.service.js
@@ -2,24 +2,31 @@ const Order = require("../models/Order");
 const Product = require("../models/Product");
 const User = require("../models/User");
 
-const createOrder = async (request) => {
-
-    //  console.log(request.user);
-    const usrID = request.user.id;
-    const prodID = request.params.pid;
-
-    request.body.status = "in order";
-    request.body.user_id = usrID;
-    request.body.product_id = prodID;
-    const order = Order.build(request.body);
-    
-    console.log("Order saved");
+const createOrder = async (pid, uid) => {
+    request = {}
+    request.status = "in order";
+    request.user_id = uid;
+    request.product_id = pid;
+    const order = Order.build(request);
     
     await order.save();
+}
 
-    //console.log(usrID);
+const fetchOrderByProdAndUser = async (pid, uid) => {
+    return await Order.findOne({where: {product_id: pid, user_id: uid}}).then(function (prod){
+        if(!prod)
+            return null;
+        else
+            return prod.dataValues;
+    });
+}
+
+const fetchOrderByUser = async (uid) => {
+    return await Order.findAll({where: {user_id: uid}}).then(function(o) {
+        return o.map(function(obj) {return obj.dataValues});
+    });
 }
 
 module.exports = {
-    createOrder
+    createOrder, fetchOrderByProdAndUser, fetchOrderByUser
 }

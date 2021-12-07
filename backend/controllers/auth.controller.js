f680e68d0c756610de3525f54fa243c9e6ed94da
Daniel <danibociat@gmail.com>
2021-12-07 11:42:43 +0200

    Improved register/login + fixed bugs

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,34 +9,53 @@ const accessTokenSecret = 'youraccesstokensecret';
 
 const register = async (req, res, next) => {
 	const { first_name, last_name, email, password, address, phone_number, gender, dob } = req.body;
+	var error = {status:"", error:[]};
 	if (!first_name) {
-		res.status(400);
-		return res.send("first_name is required");
+		error.error.push( "first name is required");
 	}
 	if (!last_name) {
-		res.status(400);
-		return res.send("last_name is required");
+		error.error.push( "last name is required");
 	}
 	if (!email) {
-		res.status(400);
-		return res.send("email is required");
+		error.error.push( "email is required");
 	}
 	if (!address) {
-		res.status(400);
-		return res.send("address is required");
+		error.error.push( "address is required");
 	}
 	if (!phone_number) {
-		res.status(400);
-		return res.send("phone_number is required");
+		error.error.push( "phone number is required");
 	}
 	if (!gender) {
-		res.status(400);
-		return res.send("gender is required");
+		error.error.push( "gender is required");
 	}
 	if (!dob) {
-		res.status(400);
-		return res.send("dob is required");
+		error.error.push( "date of birth is required");
 	}
+	if(!password){
+		error.error.push( "password is required");
+	}
+
+	if(error.error.length){
+		error.status = 400;
+		res.status(400);
+		return res.send(error);
+	}
+
+	if(password.length <= 3){
+		error.status = 400;
+		error.error.push("password is too short");
+		res.status(400);
+		return res.send(error);
+	}
+
+	const dbuser = await User.findOne({ where: { email: email}});
+	if(dbuser){
+		error.status = 400;
+		error.error.push("Email already in use");
+		res.status(400);
+		return res.send(error);
+	}
+
 	try {
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
@@ -64,35 +83,44 @@ const register = async (req, res, next) => {
         });
 		next();
 	} catch (e) {
-		console.log(e.message);
-		res.sendStatus(500) && next(e);
+		error.status = 500;
+		error.error.push(e.message);
+		res.status(500);
+		
+		return res.send(error);
 	}
 };
 
 const login = async (req, res, next) => {
+	var error = {status:"", error:[]};
 	const { email, password } = req.body;
 	if (!email) {
-		res.status(400);
-		return res.send("email is required");
+		error.error.push("Email is required");
 	}
 	if (!password) {
-		res.status(400);
-		return res.send("password is required");
+		error.error.push("Email is required");
 	}
+
+	if(error.error.length){
+		res.status(400);
+		error.status = 400;
+		return res.send(error);
+	}
+
 	try {
 		const dbuser = await User.findOne({ where: { email: email } });
-		if(!dbuser) {
-			res.sendStatus(404);
-			next();
+		if(!dbuser)
+			error.error.push("Invalid email or password");
+		
+		if(!error.error.length)
+		{
+			const validPassword = await bcrypt.compare(password, dbuser.password);
+			if(!validPassword)
+				error.error.push("Invalid email or password");
 		}
-		console.log(dbuser);
-		const validPassword = await bcrypt.compare(password, dbuser.password);
-		if(!validPassword){
-			res.status(403);
-			return res.send("Invalid password");
-		}
-		const user = { id: dbuser.id, username: dbuser.email };
-		if (dbuser) {
+		
+		if(!error.error.length){
+			const user = { id: dbuser.id, username: dbuser.email };
 			const accessToken = jwt.sign(
 				user, 
 				accessTokenSecret, {
@@ -105,22 +133,32 @@ const login = async (req, res, next) => {
 				accessToken,
 				refreshToken
 			});
-		} else {
-			res.send('Username or password incorrect');
+		}
+		else{
+			res.status(404);
+			error.status = 404;
+			return res.send(error);
 		}
 	} catch (e) {
-		console.log(e.message);
-		res.sendStatus(500) && next(e);
+		error.status = 500;
+		error.error = e.message;
+		res.status(500);
+		
+		return res.send(error);
 	}
 };
 
 const token = async (req, res, next) => {
+	var error = {status:"", error:[]};
 	try {
 		res.status(200);
 		return res.send(await fetchUserByID(req.user.id));
 	} catch (e) {
-		console.log(e.message);
-		res.sendStatus(400);
+		error.status = 400;
+		error.error = e.message;
+		res.status(400);
+		
+		return res.send(error);
 	}
 }
 

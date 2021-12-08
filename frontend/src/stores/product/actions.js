25ba31d0d0d6a2d0d4bce07592ab234efe75402e
ValentinRL <valentin.alt.raltchev99@gmail.com>
2021-12-08 14:52:43 +0100

    New store and Fixes

---
+++ frontend/src/stores/product/actions.js
@@ -0,0 +1,9 @@
+import * as C from './creators';
+
+export const addProduct = () => async (dispatch) => {
+	try {
+	    //TODO
+	} catch (error) {
+	    //TODO
+	}
+};
---
+++ frontend/src/stores/product/reducer.js
@@ -0,0 +1,15 @@
+import Types from '../types';
+
+const initialState = {
+    products: null,
+    error: undefined,
+};
+
+const reducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;

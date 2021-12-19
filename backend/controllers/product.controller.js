679f1461f25cb9eb5f3d47fd9e6056a5aeb59cd4
Valentin <valentin.alt.raltchev99@gmail.com>
2021-12-19 17:04:01 +0200

    Fixes

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,7 +1,7 @@
 const { connectionManager } = require("../config/db");
 const Product = require("../models/Product");
 const { use } = require("../routes");
-const {createProduct, fetchProduct, fetchOwnProducts, fetchAvailableProducts, fetchProductByWord, fetchProductById, fetchProductByCategory, removeProduct, editProduct} = require("../services/product.service")
+const {createProduct, fetchProduct, fetchOwnProducts, fetchAvailableProducts, fetchProductByWord, fetchProductById, fetchProductByCategory, removeProduct, editProduct, getUserOfProduct} = require("../services/product.service")
 
 const addProduct = async (req, res, next) => {
     var error = {status:"", error:[]};
@@ -115,16 +115,13 @@ const deleteProduct = async (req, res, next) => {
   var error = {status:"", error:[]};
 	try {
     prod = await fetchProductById(req.params.pid, false);
-    console.log(prod);
     if(!prod){
       error.status = 404;
       res.status(404);
       error.error.push("Product does not exist");
       return res.send(error);
     }
-    console.log(prod.user_id)
-    console.log(req.user.id)
-    if(prod.user_id == req.user.id || req.user.isAdmin){
+    if(await getUserOfProduct(pid) == req.user.id || req.user.isAdmin){
       await removeProduct(req.params.pid);
       res.sendStatus(200);
     }
